fe17e1af981c2e3a7f1a525dde8a0b71f14b9573
R-Currie <robert.currie1019@gmail.com>
2019-09-06 21:10:23 +0000

    fixed edit form

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -112,15 +112,6 @@ router.get('/:id/edit', middleware.checkCampgroundOwnership, function(req, res){
 
 //UPDATE
 router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
-  geocoder.geocode(req.body.location, function (err, data) {
-    if (err || !data.length) {
-      req.flash('error', 'Invalid address');
-      return res.redirect('back');
-    }
-    req.body.campground.lat = data[0].latitude;
-    req.body.campground.lng = data[0].longitude;
-    req.body.campground.location = data[0].formattedAddress;
-
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, campground){
         if(err){
             req.flash("error", err.message);
